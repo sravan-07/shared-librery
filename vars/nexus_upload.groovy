0b92d4779caf2191b4559987dca80df5876f7594
sravan-07 <85868069+sravan-07@users.noreply.github.com>
2021-09-07 16:02:37 +0530

    Update nexus_upload.groovy

--- vars/nexus_upload.groovy
+++ vars/nexus_upload.groovy
@@ -1,4 +1,4 @@
 def call()
 {
- nexusArtifactUploader artifacts: [[artifactId: 'jb-hello-world-maven', classifier: '', file: 'target/jb-hello-world-maven-0.1.0.jar', type: 'jar']], credentialsId: 'Nexus-ID', groupId: 'org.springframework', nexusUrl: '192.168.87.128:8082', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-releases', version: '0.1.0' 
+ nexusArtifactUploader artifacts: [[artifactId: "${artifact_id}", classifier: '', file: "${file_path}", type: "${type}"]], credentialsId: 'Nexus-ID', groupId: "${group_id}", nexusUrl: '192.168.87.128:8082', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-releases', version: "${version}" 
 }
