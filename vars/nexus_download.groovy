10e8e2ee82f7dbab6c7f99dbbc81f932069c404b
sravan-07 <85868069+sravan-07@users.noreply.github.com>
2021-09-07 18:42:10 +0530

    Update nexus_download.groovy

--- vars/nexus_download.groovy
+++ vars/nexus_download.groovy
@@ -1,4 +1,5 @@
 def call()
 {
   sh "curl http://192.168.87.128:8082/repository/maven-releases/${group_id}/${artifact_id}/${version}/${artifact_id}-${version}.${type} -o ${artifact_id}-${version}.${type}"
+  sshPublisher(publishers: [sshPublisherDesc(configName: 'test-tomcat', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: '', sourceFiles: 'sparkjava-hello-world-1.3.war')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
 }
